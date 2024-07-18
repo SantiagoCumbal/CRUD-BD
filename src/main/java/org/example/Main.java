5f41ab726fdfb87149e13dbb01c18d2a4c9ddeb1
1-21-01-E043-09\POO <santiago.cumbalepn.edu.ec>
2024-07-18 08:00:42 -0500

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,48 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String url = "jdbc:mysql://localhost:3306/EstudiantesA24";
+        String usser = "root";
+        String pass = "123456";
+        PreparedStatement pstmt=null;
+        Connection connection =null;
+        try {
+            connection = DriverManager.getConnection(url,usser,pass);
+            System.out.println("Conexion establecida");
+            String sql="Update estudiantesA set b1 = ? where cedula = ?";
+            pstmt=connection.prepareStatement(sql);
+            //seteamos los valores de la sentencia previa
+            pstmt.setInt(1,0);
+            pstmt.setString(2,"3300010840");
+            pstmt.executeUpdate();
+
+            //cuenta las lineas de codigo
+            int n=pstmt.getUpdateCount();
+            System.out.println("Se modificaron estas lineas de codigo: "+n);
+
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        } finally {
+            //Cerrar la conexion
+            try{
+                if(pstmt!=null){
+                    pstmt.close();
+                }
+                if (connection!=null){
+                    connection.close();
+                }
+            }catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
         }
+
     }
 }
